6529059fb5a4fcde5de251a54acd8c7c2094e7dd
TomokiCHIBA <michiamo.t1@gmail.com>
2025-09-15 14:09:06 +0000

    feat: MSW導入とVitestと組み合わせたサンプルテスト実装

---
+++ tests/unit/msw.test.tsx
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+
+test("responds with the user", async () => {
+  const response = await fetch("https://api.example.com/user");
+
+  await expect(response.json()).resolves.toEqual({
+    id: "abc-123",
+    firstName: "John",
+    lastName: "Maverick",
+  });
+});
--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,9 +1,13 @@
 import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { server } from "./mocks/node.js";
 
 export const user = userEvent.setup();
 
+afterAll(() => server.close());
+beforeAll(() => server.listen());
 afterEach(() => {
+  server.resetHandlers();
   cleanup();
 });
